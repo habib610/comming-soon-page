d83dfb792325e5ae8cce0d30fab1da5f04d79938
habib610 <habib54562@gmail.com>
2020-07-30 17:59:46 +0600

    comming soon page donae

---
+++ Dom/js/script.js
@@ -0,0 +1,6 @@
+console.dir(document);
+console.log(document.title);
+document.title = "change";
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
---
+++ comming-soon/script/script.js
@@ -0,0 +1,23 @@
+const countDown = document.querySelector('.countDown');
+console.log(countDown);
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+
+const launchDate = new Date('Jan 1, 2022 19:00:00').getTime();
+console.log(launchDate);
+const intrvl = setInterval(function(){
+   const now = new Date().getTime();
+   const distance = launchDate - now;
+   const days =Math.floor(distance / (1000 * 60 * 60 * 24));
+   const hours = Math.floor(distance % (1000 * 60 * 60 * 24)/(1000 * 60 * 60));
+   const minutes = Math.floor(distance % (1000 * 60 * 60 )/(1000 * 60));
+   const seconds = Math.floor(distance % (1000 * 60 ) / 1000);
+   
+   document.getElementById('days').innerText =days;
+   document.getElementById('hours').innerText = hours;
+   document.getElementById('minutes').innerText = minutes;
+   document.getElementById('seconds').innerText = seconds;
+}, 1000)
